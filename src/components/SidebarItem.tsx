b34dafa4c2639236deb8a9ee54672a36e1c97be1
crystaltine <donaldcenaaa@outlook.com>
2026-02-21 15:52:09 -0800

    tweak title style of SidebarItem

--- src/components/SidebarItem.tsx
+++ src/components/SidebarItem.tsx
@@ -9,8 +9,12 @@ type SidebarItemProps = {
 
 export const SidebarItem = (props: SidebarItemProps) => {
 	return (
-		<div className='flex flex-col gap-2'>
-			<h3 className='text-sm font-medium'>[{props.label}]</h3>
+		<div className='flex flex-col gap-2 p-2 border border-border bg-secondary'>
+			<div className='flex gap-2 items-center mb-2'>
+				<hr className='flex-1 border-muted-foreground' />
+				<h3 className='flex-grow-0 text-sm font-medium text-secondary-foreground'>[{props.label}]</h3>
+				<hr className='flex-1 border-muted-foreground' />
+			</div>
 			{props.children}
 		</div>
 	);
