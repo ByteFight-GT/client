8f2f9191b9dcc6a6b571917e2cc9a7e4e4bb7c9c
crystaltine <donaldcenaaa@outlook.com>
2026-02-21 22:40:01 -0800

    add builtin loading style to Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,31 +2,32 @@ import React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
+import { LoadingSpinner } from "./LoadingSpinner"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition duration-100 cursor-default hover:brightness-[80%] active:brightness-[70%] disabled:pointer-events-none disabled:brightness-50 disabled:saturate-80 [&_svg.lucide]:text-muted-foreground",
   {
-  variants: {
-    variant: {
-    default: "bg-primary text-primary-foreground hover",
-    destructive: "bg-destructive text-destructive-foreground",
-    outline: "border border-border bg-background text-foreground",
-    secondary: "border border-border bg-secondary text-foreground",
-    ghost: "bg-transparent text-foreground hover:bg-secondary hover:text-secondary-foreground",
-    link: "bg-transparent text-muted-foreground underline underline-offset-2",
+    variants: {
+      variant: {
+        default: "bg-primary text-primary-foreground hover",
+        destructive: "bg-destructive text-destructive-foreground",
+        outline: "border border-border bg-background text-foreground",
+        secondary: "border border-border bg-secondary text-foreground",
+        ghost: "bg-transparent text-foreground hover:bg-secondary hover:text-secondary-foreground",
+        link: "bg-transparent text-muted-foreground underline underline-offset-2",
+      },
+      size: {
+        default: "px-3 py-1",
+        sm: "px-2",
+        lg: "h-11 px-8",
+        icon: "h-10 w-10 p-0",
+        iconsm: "h-8 w-8 p-0",
+      },
     },
-    size: {
-    default: "px-3 py-1",
-    sm: "px-2",
-    lg: "h-11 px-8",
-    icon: "h-10 w-10 p-0",
-    iconsm: "h-8 w-8 p-0",
+    defaultVariants: {
+      variant: "default",
+      size: "default",
     },
-  },
-  defaultVariants: {
-    variant: "default",
-    size: "default",
-  },
   }
 )
 
@@ -34,18 +35,32 @@ export interface ButtonProps extends
 	React.ButtonHTMLAttributes<HTMLButtonElement>,
 	VariantProps<typeof buttonVariants> 
 {
-  asChild?: boolean
+  asChild?: boolean;
+  loading?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, loading = false, children, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
-    return (
+    return loading? (
+      <Comp
+      ref={ref}
+      disabled
+      className={cn("relative", buttonVariants({ variant, size, className }))}
+      {...props}>
+        {children}
+        <div className="absolute-cover backdrop-brightness-50 flex items-center justify-center">
+          <LoadingSpinner />
+        </div>
+      </Comp>
+    ) : (
       <Comp
 			ref={ref}
 			className={cn(buttonVariants({ variant, size, className }))}
-			{...props} />
-    )
+			{...props}>
+        {children}
+      </Comp>
+    );
   }
 )
 Button.displayName = "Button"
---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React from 'react';
+
+import { cn } from '@/lib/utils';
+
+export const LoadingSpinner = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    viewBox="0 0 100 100" 
+    preserveAspectRatio="xMidYMid" 
+    className={cn('w-4 h-4', props.className)}
+    style={{
+      shapeRendering: "auto",
+      display: "block",
+      background: "transparent"
+    }}>
+      <g>
+        <circle strokeDasharray="164.93361431346415 56.97787143782138" r="35" strokeWidth="10" stroke="currentColor" fill="none" cy="50" cx="50">
+          <animateTransform keyTimes="0;1" values="0 50 50;360 50 50" dur="1s" repeatCount="indefinite" type="rotate" attributeName="transform"></animateTransform>
+        </circle>
+      </g>
+    </svg>
+  );
+};
