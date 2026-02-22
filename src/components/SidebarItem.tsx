20f37cb8455bc3a20c587a4bdb9f6537a64a2f2d
crystaltine <donaldcenaaa@outlook.com>
2026-02-21 19:26:03 -0800

    add support for custom labels in SidebarItem

--- src/components/SidebarItem.tsx
+++ src/components/SidebarItem.tsx
@@ -3,18 +3,25 @@
 import React from 'react';
 
 type SidebarItemProps = {
-	label: string;
+	label: string | React.ReactNode; // thing in the header
+	disableDefaultHeader?: boolean; // whether to include the default header styling (brackets and lines)
 	children: React.ReactNode;
 };
 
 export const SidebarItem = (props: SidebarItemProps) => {
 	return (
 		<div className='flex flex-col gap-2 p-2 border border-border bg-secondary'>
-			<div className='flex gap-2 items-center mb-2'>
-				<hr className='flex-1 border-muted-foreground' />
-				<h3 className='flex-grow-0 text-sm font-medium text-secondary-foreground'>[{props.label}]</h3>
-				<hr className='flex-1 border-muted-foreground' />
-			</div>
+
+			{props.disableDefaultHeader?
+				props.label
+			:
+				<div className='flex gap-2 items-center'>
+					<hr className='flex-1 border-muted-foreground' />
+					<h3 className='flex-grow-0 text-sm font-medium text-secondary-foreground'>[{props.label}]</h3>
+					<hr className='flex-1 border-muted-foreground' />
+				</div>
+			}
+
 			{props.children}
 		</div>
 	);
