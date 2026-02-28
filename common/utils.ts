a5e2d2ad958deefd1eeb202278893b24b662c553
crystaltine <donaldcenaaa@outlook.com>
2026-02-28 00:18:49 -0800

    add pluralization func to utils

--- common/utils.ts
+++ common/utils.ts
@@ -37,3 +37,36 @@ export function generateMatchId(queuedTime: number): string {
   const randomSuffix = Math.random().toString(36).substring(2, 7); // 5 random alphanumeric chars
   return `${queuedTime}-${randomSuffix}`;
 }
+
+/** returns `{count} {plural}` if count != 1 otherwise `{count} {singular}` */
+export function word(count: number, singular: string, plural: string): string {
+  return `${count} ${count === 1? singular : plural}`;
+}
+
+/**
+ * gets the time elapsed since `timestamp` (unix ms) in an english string.
+ * Form:
+ * - x (seconds|minutes|hours|days|weeks|months|years) ago
+ * 
+ * Months are counted as 30 days, years are counted as 365 days
+ */
+export function timeElapsedString(timestamp: number): string {
+  const now = Date.now();
+  const elapsedSeconds = Math.floor((now - timestamp) / 1000);
+
+  const minutes = Math.floor(elapsedSeconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const weeks = Math.floor(days / 7);
+  const months = Math.floor(days / 30);
+  const years = Math.floor(days / 365);
+
+  if (years > 0) return `${word(years, 'year', 'years')} ago`;
+  if (months > 0) return `${word(months, 'month', 'months')} ago`;
+  if (weeks > 0) return `${word(weeks, 'week', 'weeks')} ago`;
+  if (days > 0) return `${word(days, 'day', 'days')} ago`;
+  if (hours > 0) return `${word(hours, 'hour', 'hours')} ago`;
+  if (minutes > 0) return `${word(minutes, 'minute', 'minutes')} ago`;
+  
+  return `${word(elapsedSeconds, 'second', 'seconds')} ago`;
+}
