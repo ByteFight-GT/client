a35202ec19dea1e34bbfc43ab59e5629d1f756df
crystaltine <donaldcenaaa@outlook.com>
2026-02-28 10:55:02 -0800

    add queue tab

--- src/app/player/components/QueueTab.tsx
+++ src/app/player/components/QueueTab.tsx
@@ -1,11 +1,30 @@
 "use client";
 
+import { useRunner } from '@/hooks/useRunner';
 import React from 'react';
+import { QueuedMatchCard } from './QueuedMatchCard';
+import { word } from '../../../../common/utils';
+import { Button } from '@/components';
 
 export const QueueTab = () => {
+
+  const {queuedMatches, clearAllQueued} = useRunner();
+
   return (
-    <div>
-      queue tab
+    <div className='flex flex-col gap-4 p-4'>
+      <div>
+        <h2>Queue</h2>
+        <p className='text-muted-foreground'>{word(queuedMatches.length, "match", "matches")} waiting</p>
+        <Button className='mt-2 w-full' variant="destructive" onClick={clearAllQueued} disabled={queuedMatches.length === 0}>
+          Clear All Queued Matches
+        </Button>
+      </div>
+
+      <hr />
+
+      {queuedMatches.map((match, index) => (
+        <QueuedMatchCard key={match.matchId} queueIndex={index} matchData={match} />
+      ))}
     </div>
   );
 };
---
+++ src/app/player/components/QueuedMatchCard.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import React from 'react';
+import { MatchMetadata } from '../../../../common/types';
+import { timeElapsedString, word } from '../../../../common/utils';
+import { Button } from '@/components';
+import { useRunner } from '@/hooks/useRunner';
+import { ChevronDownIcon, ChevronsUpIcon, ChevronUpIcon } from 'lucide-react';
+import Image from 'next/image';
+
+type QueuedMatchCardProps = {
+  queueIndex: number;
+  matchData: MatchMetadata;
+};
+
+/** 
+ * displays MatchMetadata objects specifically for the queue page
+ * Please make sure `matchData` is actually from the queue!!!
+ */
+export const QueuedMatchCard = (props: QueuedMatchCardProps) => {
+
+  // stored in state so we can update every min
+  const [timeElapsedDisplay, setTimeElapsedDisplay] = React.useState<string>(timeElapsedString(props.matchData.queuedTimestamp));
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeElapsedDisplay(timeElapsedString(props.matchData.queuedTimestamp));
+    }, 60000);
+
+    return () => clearInterval(interval);
+  }, [props.matchData.queuedTimestamp]);
+
+  const {queuedMatches, moveWithinQueue, dequeueMatch} = useRunner();
+
+  const promoteToFront = React.useCallback(() => {
+    if (props.queueIndex === 0) return;
+    moveWithinQueue(props.queueIndex, 0);
+  }, [props.queueIndex, moveWithinQueue]);
+
+  const moveForward = React.useCallback(() => {
+    if (props.queueIndex === 0) return;
+    moveWithinQueue(props.queueIndex, props.queueIndex - 1);
+  }, [props.queueIndex, moveWithinQueue]);
+
+  const moveBackward = React.useCallback(() => {
+    if (props.queueIndex === queuedMatches.length - 1) return;
+    moveWithinQueue(props.queueIndex, props.queueIndex + 1);
+  }, [props.queueIndex, queuedMatches.length, moveWithinQueue]);
+
+  return (
+    <div className={`match-card ${props.queueIndex === 0? "border-primary" : "border-border"}`}>
+      <div>
+        <h3 className='match-card-green-header ellipsis' title={props.matchData.teamGreen}>
+          <Image className='inline' src="/green_team_icon.svg" alt="*" width={12} height={12} />&nbsp;
+          {props.matchData.teamGreen}
+        </h3>
+        <h3 className='match-card-blue-header ellipsis' title={props.matchData.teamBlue}>
+          <Image className='inline' src="/blue_team_icon.svg" alt="*" width={12} height={12} />&nbsp;
+          {props.matchData.teamBlue}
+        </h3>
+      </div>
+
+      <div>
+        <h4 className='text-sm text-muted-foreground'>
+          On {word(props.matchData.maps.length, "map", "maps")}:
+        </h4>
+        {props.matchData.maps.map((map, index) => (
+          <p key={index} className='text-sm'>{map}</p>
+        ))}
+      </div>
+
+      <hr />
+
+      <div>
+        <p className='text-sm text-muted-foreground'>
+          Queue Position&nbsp;
+          <span className='text-foreground font-bold'>
+            {props.queueIndex > 0? `#${props.queueIndex + 1}` : "UP NEXT"}
+          </span>
+        </p>
+        <p className='text-sm text-muted-foreground' title={new Date(props.matchData.queuedTimestamp).toLocaleString()}>
+          Queued&nbsp;
+          <span className='text-secondary-foreground'>{timeElapsedDisplay}</span>
+        </p>
+        <p className='text-sm text-muted-foreground'>
+          Match ID&nbsp;
+          <span className='text-secondary-foreground'>{props.matchData.matchId}</span>
+        </p>
+        <div className='flex flex-wrap gap-2 mt-2'>
+          <Button variant="outline" size="iconsm" onClick={promoteToFront}><ChevronsUpIcon /></Button>
+          <Button variant="outline" size="iconsm" disabled={props.queueIndex === 0} onClick={moveForward}><ChevronUpIcon /></Button>
+          <Button variant="outline" size="iconsm" disabled={props.queueIndex === queuedMatches.length - 1} onClick={moveBackward}><ChevronDownIcon /></Button>
+          <Button variant="destructive" onClick={() => dequeueMatch(props.queueIndex)}>Cancel</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
