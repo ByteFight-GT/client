019497b8afd6f5e9f7b93b4e0b2b632ff8b729c9
crystaltine <donaldcenaaa@outlook.com>
2026-03-01 21:08:25 -0800

    add pagination func to matches page

--- src/app/matches/page.tsx
+++ src/app/matches/page.tsx
@@ -5,11 +5,16 @@ import "./page.css";
 
 import { useMatches } from '@/hooks/useMatches';
 import { CompletedMatchCard } from './components/CompletedMatchCard';
-import { GenericPage } from '@/components';
+import { Button, GenericPage } from '@/components';
+import { word } from '../../../common/utils';
+import { useLoadings } from '@/hooks/useLoadings';
+
+const PAGE_SIZE = 30;
 
 const MatchReplayerPage = () => {
 
-	const {completedMatchHistory} = useMatches();
+	const {loadings} = useLoadings();
+	const {completedMatchHistory, totalMatchesIndexed, fetchMatchHistoryNextPage} = useMatches();
 
 	console.log("match replayer page: matches: ", completedMatchHistory);
 
@@ -17,11 +22,29 @@ const MatchReplayerPage = () => {
 		<GenericPage 
 		variant="thin"
 		className='relative flex flex-col pb-8' 
-		titleEle={<span className='text-primary leading-normal'>Matches</span>}>
+		titleEle={<span className='text-primary leading-normal'>Matches</span>}
+		subtitleEle={
+			<span className='flex gap-4 items-center'>
+				{word(totalMatchesIndexed, "match", "matches")} played
+				&middot;
+				<Button variant="secondary">Import from file</Button>
+			</span>
+		}>
 			<div className='matches-list'>
 				{completedMatchHistory.map(match => (
 					<CompletedMatchCard key={match.matchId} matchData={match} />
 				))}
+				{completedMatchHistory.length < totalMatchesIndexed?
+					<Button
+					className='mx-auto'
+					disabled={loadings.fetchMatchHistoryNextPage}
+					variant="outline"
+					onClick={() => fetchMatchHistoryNextPage(PAGE_SIZE)}>
+						Load More ({totalMatchesIndexed} / {completedMatchHistory.length})
+					</Button>
+				:
+					<p className='text-center text-sm text-muted-foreground'>That's all folks!</p>
+				}
 			</div>
 		</GenericPage>
 	);
