2103ecece577e142882753bcff357c947836894a
crystaltine <donaldcenaaa@outlook.com>
2026-03-07 14:24:42 -0800

    make button icons not be muted-foreground in default or destructive variants

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,16 +6,16 @@ import { LoadingSpinner } from "./LoadingSpinner"
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./Tooltip"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition duration-100 cursor-default [&:not(:disabled)]:hover:brightness-[80%] [&:not(:disabled)]:active:brightness-[70%] disabled:brightness-50 disabled:saturate-80 [&_svg.lucide]:text-muted-foreground",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition duration-100 cursor-default [&:not(:disabled)]:hover:brightness-[80%] [&:not(:disabled)]:active:brightness-[70%] disabled:brightness-50 disabled:saturate-80",
   {
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground",
         destructive: "bg-destructive text-destructive-foreground",
-        outline: "border border-border bg-background text-foreground",
-        secondary: "border border-border bg-secondary text-foreground",
-        ghost: "bg-transparent text-foreground [&:not(:disabled)]:hover:bg-secondary [&:not(:disabled)]:hover:text-secondary-foreground",
-        link: "bg-transparent text-muted-foreground underline underline-offset-2",
+        outline: "border border-border bg-background text-foreground [&_svg.lucide]:text-muted-foreground",
+        secondary: "border border-border bg-secondary text-foreground [&_svg.lucide]:text-muted-foreground",
+        ghost: "bg-transparent text-foreground [&:not(:disabled)]:hover:bg-secondary [&:not(:disabled)]:hover:text-secondary-foreground [&_svg.lucide]:text-muted-foreground",
+        link: "bg-transparent text-muted-foreground underline underline-offset-2 [&_svg.lucide]:text-muted-foreground",
       },
       size: {
         default: "px-3 py-1",
