98ade27660f12cbc57f4bafb8b8847a516d6688d
crystaltine <donaldcenaaa@outlook.com>
2026-03-07 19:59:27 -0800

    add background image to game window

--- src/app/player/components/GameWindow.tsx
+++ src/app/player/components/GameWindow.tsx
@@ -2,20 +2,17 @@
 
 import React from 'react';
 import { GameNavigator } from './GameNavigator';
-import { GameRenderer } from '@/gamerenderer/GameRenderer';
+import { GameRenderer } from '@/gamerenderer/GameRenderer'; 
 
-type GameWindowProps = {
-	
-};
-
-export const GameWindow = (props: GameWindowProps) => {
+export const GameWindow = () => {
 	return (
 		<div className='GameWindow-container'>
 			<GameRenderer transformComponentProps={{
 				wrapperStyle: { 
 					width: '100%', 
 					height: '100%',
-					backgroundColor: "hsl(230, 45%, 15%)", // temp color for now. TODO - add background image?
+					backgroundImage: `url(/gamerenderer-bg.webp)`,
+					backgroundSize: 'cover',
 				},
 			}} />
 			<GameNavigator />
